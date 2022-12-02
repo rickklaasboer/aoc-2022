6183343d5e1ab063cd0679bc0b3a3daa1b5f766e
Rick Klaasboer <rick@klaasboer.org>
2022-12-02 11:51:43 +0100

    add day 2

---
+++ day-2/index.ts
@@ -0,0 +1,54 @@
+import fs from "fs";
+import readline from "readline";
+
+const POSSIBLE_OUTCOMES_PART_ONE = {
+  "A X": 4,
+  "A Y": 8,
+  "A Z": 3,
+
+  "B X": 1,
+  "B Y": 5,
+  "B Z": 9,
+
+  "C X": 7,
+  "C Y": 2,
+  "C Z": 6,
+};
+
+const POSSIBLE_OUTCOMES_PART_TWO = {
+  "A X": 3,
+  "A Y": 4,
+  "A Z": 8,
+
+  "B X": 1,
+  "B Y": 5,
+  "B Z": 9,
+
+  "C X": 2,
+  "C Y": 6,
+  "C Z": 7,
+};
+
+async function calculateScore(input: Record<string, number>) {
+  const lines = readline.createInterface({
+    input: fs.createReadStream(__dirname + "/data.txt"),
+    crlfDelay: Infinity,
+  });
+
+  let score = 0;
+
+  for await (const line of lines) {
+    const key = line as keyof typeof input;
+    score += input[key];
+  }
+
+  return score;
+}
+
+// { partOne: 14375 }
+// { partTwo: 10274 }
+
+(async () => {
+  console.log({ partOne: await calculateScore(POSSIBLE_OUTCOMES_PART_ONE) });
+  console.log({ partTwo: await calculateScore(POSSIBLE_OUTCOMES_PART_TWO) });
+})();

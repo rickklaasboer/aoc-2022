73244d328cf5a894434bc549a8373619bd42b79c
Rick Klaasboer <rick@klaasboer.org>
2022-12-04 13:52:48 +0100

    add day 4

---
+++ day-4/index.ts
@@ -0,0 +1,54 @@
+import { createReadlineInterface, intersect, range } from "../util";
+
+/**
+ * Split lines by ',' and '-' and create ranges from resulting arrays
+ */
+function rangeFromLine(line: string): [number[], number[]] {
+  const [part1, part2] = line.split(",");
+  const [first, second] = [
+    part1.split("-").map((x) => parseInt(x)),
+    part2.split("-").map((x) => parseInt(x)),
+  ];
+
+  return [range(first[0], first[1]), range(second[0], second[1])];
+}
+
+async function partOne() {
+  const lines = createReadlineInterface(__dirname + "/data.txt");
+
+  let sum = 0;
+
+  for await (const line of lines) {
+    const [range1, range2] = rangeFromLine(line);
+
+    if (
+      range1.every((x) => range2.includes(x)) ||
+      range2.every((x) => range1.includes(x))
+    ) {
+      sum++;
+    }
+  }
+
+  return sum;
+}
+
+async function partTwo() {
+  const lines = createReadlineInterface(__dirname + "/data.txt");
+
+  let sum = 0;
+
+  for await (const line of lines) {
+    const [range1, range2] = rangeFromLine(line);
+
+    if (intersect(range1, range2).length > 0) {
+      sum++;
+    }
+  }
+
+  return sum;
+}
+
+// { partOne: 584, partTwo: 933 }
+(async () => {
+  console.log({ partOne: await partOne(), partTwo: await partTwo() });
+})();
--- util.ts
+++ util.ts
@@ -7,3 +7,23 @@ export function createReadlineInterface(path: string): readline.Interface {
     crlfDelay: Infinity,
   });
 }
+
+/**
+ * Intersect arrays
+ */
+export function intersect<T1 extends Array<number>, T2 extends Array<number>>(
+  arr1: T1,
+  arr2: T2
+): Array<number> {
+  return arr1.filter((x) => arr2.includes(x));
+}
+
+/**
+ * Create range from 0...n
+ */
+export function range(start: number, stop: number, step: number = 1) {
+  return Array.from(
+    { length: (stop - start) / step + 1 },
+    (_, i) => start + i * step
+  );
+}
